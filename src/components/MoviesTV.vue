42b6524070b59b8881213b55567464dd3ac4c05d
Simone823 <simone.daglio823@outlook.it>
2022-03-16 00:32:17 +0100

    tv_wrapper, tvArray props

--- src/components/MoviesTV.vue
+++ src/components/MoviesTV.vue
@@ -3,26 +3,49 @@
     <!-- Wrapper -->
     <div class="wrapper">
 
-        <!-- Movie card -->
-        <div class="movie_card" v-for="element in moviesArray" :key="element.id">
+        <!-- Wrapper movie -->
+        <div class="wrapper_movie">
 
-            <!-- Img wrapper -->
-            <div class="img_wrapper">
-               <img src="https://picsum.photos/200" alt="">
+            <!-- Movie card -->
+            <div class="movie_card" v-for="element in moviesArray" :key="element.id">
+                <!-- Img wrapper -->
+                <div class="img_wrapper">
+                <img src="https://picsum.photos/200" alt="">
+                </div>
+
+                <!-- Info wrapper -->
+                <div class="info">
+                    <h1 class="title">{{element.title}}</h1>
+                    <p class="original_title">{{element.original_title}}</p>
+                    <p class="language">{{element.original_language}}</p>
+                    <p class="vote">{{element.vote_average}}</p>
+                </div>
             </div>
+        </div>
 
-            <!-- Info wrapper -->
-            <div class="info">
-                <h1 class="title">{{element.title}}</h1>
-                <p class="original_title">{{element.original_title}}</p>
-                <p class="language">{{element.original_language}}</p>
-                <p class="vote">{{element.vote_average}}</p>
+        <!-- Serie tv wrapper -->
+        <div class="tv_wrapper">
+
+            <!-- Tv card -->
+            <div class="tv_card" v-for="element in tvArray" :key="element.id">
+                <!-- Img wrapper -->
+                <div class="img_wrapper">
+                    <img src="" alt="">
+                </div>
+
+                <!-- Info wrapper -->
+                <div class="info_wrapper">
+                    <h2 class="name">{{element.name}}</h2>
+                    <p class="original_name">{{element.original_name}}</p>
+                    <p class="original_language">{{element.original_language}}</p>
+                    <p class="vote">{{element.vote}}</p>
+                </div>
             </div>
 
         </div>
 
         <!-- Loading -->
-        <div class="loading" :class="moviesArray.length == 0 ? 'active' : '' ">
+        <div class="loading" :class="moviesArray.length == 0 &&  tvArray.length == 0 ? 'active' : '' ">
             <div class="circle"></div>
             <h2>Nella barra di ricerca scrivi un film o una serie tv e premi invio</h2>
         </div>
@@ -40,6 +63,9 @@ export default {
     props: {
         // arrayMovies
         moviesArray: Array,
+
+        // arrayTv
+        tvArray: Array,
     }
 }
 </script>
@@ -52,40 +78,86 @@ export default {
 .wrapper {
     width: 100%;
     display: flex;
-    flex-wrap: wrap;
-    gap: 25px;
+    flex-direction: column;
     row-gap: 35px;
 
-    .movie_card {
+    .wrapper_movie {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 25px;
+        row-gap: 35px;
+
+        .movie_card {
         width: calc(100% / 6 - 25px);
 
-        .img_wrapper {
-            width: 100%;
-            height: 380px;
-            aspect-ratio: 1/1;
-            margin-bottom: 20px;
-        }
-
-        .info {
-            padding: 0 5px;
-
-            .title {
-                font-size: 20px;
-                text-shadow: 0 0 5px black;
-                margin-bottom: 5px;
+            .img_wrapper {
+                width: 100%;
+                height: 380px;
+                aspect-ratio: 1/1;
+                margin-bottom: 20px;
             }
 
-            .original_title {
-                color: $color-green;
-                text-shadow: 0 0 5px black;
-            }
+            .info {
+                padding: 0 5px;
 
-            .language {
-                margin-bottom: 8px;
-                font-size: 18px;
+                .title {
+                    font-size: 20px;
+                    text-shadow: 0 0 5px black;
+                    margin-bottom: 5px;
+                }
+
+                .original_title {
+                    color: $color-green;
+                    text-shadow: 0 0 5px black;
+                }
+
+                .language {
+                    margin-bottom: 8px;
+                    font-size: 18px;
+                }
             }
         }
     }
+
+    .tv_wrapper {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 25px;
+        row-gap: 35px;
+
+        .tv_card {
+            width: calc(100% / 6 - 25px);
+
+                       .img_wrapper {
+                width: 100%;
+                height: 380px;
+                aspect-ratio: 1/1;
+                margin-bottom: 20px;
+            }
+
+            .info_wrapper {
+                padding: 0 5px;
+
+                .name {
+                    font-size: 20px;
+                    text-shadow: 0 0 5px black;
+                    margin-bottom: 5px;
+                }
+
+                .original_name {
+                    color: $color-green;
+                    text-shadow: 0 0 5px black;
+                }
+
+                .original_language {
+                    margin-bottom: 8px;
+                    font-size: 18px;
+                }
+            }
+        }
+    }
+
+
 
     .loading {
         display: none;
@@ -128,7 +200,5 @@ export default {
         }
     }
 }
-
-
 
 </style>
