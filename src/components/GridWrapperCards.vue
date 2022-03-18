190284986c2576bf7f7d53585fae8bc3bf2fa246
Simone823 <simone.daglio823@outlook.it>
2022-03-19 00:14:49 +0100

    modifica MoviesTv, divisione in componenti CardItem movies e seriestv

---
+++ src/components/GridWrapperCards.vue
@@ -0,0 +1,67 @@
+<template>
+    
+    <!-- Wrapper -->
+    <div class="grid_wrapper">
+
+        <!-- Categoria movies -->
+        <div class="categoria" v-if="moviesArray.length > 0">
+            <h1>Movies</h1>
+        </div>
+        <CardItem v-for="element in moviesArray" :key="element.id" :element="element"/>
+
+        <!-- Categoria serie tv -->
+        <div class="categoria" v-if="tvArray.length > 0">
+            <h1 class="series_tv">Series Tv</h1>
+        </div>
+        <CardItem v-for="element in tvArray" :key="element.id" :element="element"/>
+
+    </div>
+
+</template>
+
+
+
+<script>
+// Import CadItem
+import CardItem from "./CardItem.vue"
+
+export default {
+    name: "GridWrapperCards",
+
+    components: {
+        CardItem,
+    },
+
+    props: {
+        // arrayMovies componente app
+        moviesArray: Array,
+
+        // arrayTv componente app
+        tvArray: Array,
+    },
+
+}
+</script>
+
+
+<style lang="scss" scoped>
+// Import common scss
+@import '../assets/scss/common.scss';
+
+.grid_wrapper {
+    width: 100%;
+    display: flex;
+    row-gap: 80px;
+    flex-wrap: wrap;
+    gap: 35px;
+
+    .categoria {
+        width: 100%;
+        text-shadow: 0 0 5px $color-red;
+
+        .series_tv {
+            margin-top: 50px;
+        }
+    }
+}
+</style>
